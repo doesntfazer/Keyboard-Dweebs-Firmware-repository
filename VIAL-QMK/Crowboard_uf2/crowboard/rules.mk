de4a15fd203c9992a827809af94ce5a62cd86008
doesntfazer <39163867+doesntfazer@users.noreply.github.com>
2022-11-09 10:46:35 -0500

    added folder for self compilation

---
+++ VIAL-QMK/Crowboard_uf2/crowboard/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = RP2040
+
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS!
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+ENCODER_ENABLE = no
+LTO_ENABLE = no
+DYNAMIC_MACRO_ENABLE = yes
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
